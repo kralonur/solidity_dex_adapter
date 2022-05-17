5417d14f6b89807a3a0fdd460fb5b9f74bf9524a
kralonur <kralonur1998@gmail.com>
2022-05-17 15:59:14 +0700

    feat: swap exact tokens for tokens

--- contracts/Adapter.sol
+++ contracts/Adapter.sol
@@ -7,11 +7,26 @@ import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
 contract Adapter {
+    using SafeERC20 for IERC20;
+
     IUniswapV2Factory public immutable factory;
     IUniswapV2Router02 public immutable router;
 
     constructor(address _factory, address _router) {
         factory = IUniswapV2Factory(_factory);
         router = IUniswapV2Router02(_router);
+    }
+
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts) {
+        IERC20(path[0]).safeTransferFrom(msg.sender, address(this), amountIn);
+        IERC20(path[0]).approve(address(router), amountIn);
+
+        amounts = router.swapExactTokensForTokens(amountIn, amountOutMin, path, to, deadline);
     }
 }
