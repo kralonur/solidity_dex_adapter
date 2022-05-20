dd9cd65632cd0eb7ebb48f3a55a704816cb2c6d7
kralonur <kralonur1998@gmail.com>
2022-05-20 12:28:46 +0700

    refactor: remove factory from adapter and insert to unipair for testing

--- contracts/Adapter.sol
+++ contracts/Adapter.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
 /**
@@ -12,11 +11,9 @@ import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 contract Adapter {
     using SafeERC20 for IERC20;
 
-    IUniswapV2Factory public immutable factory;
     IUniswapV2Router02 public immutable router;
 
-    constructor(address _factory, address _router) {
-        factory = IUniswapV2Factory(_factory);
+    constructor(address _router) {
         router = IUniswapV2Router02(_router);
     }
 
--- contracts/test/UniPair.sol
+++ contracts/test/UniPair.sol
@@ -3,5 +3,6 @@ pragma solidity ^0.8.6;
 
 // for testing
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 
 contract UniPair {}
