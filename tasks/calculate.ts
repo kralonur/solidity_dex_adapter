91dcd0b34a3fbe30315c487bb95ba385a304ab62
kralonur <kralonur1998@gmail.com>
2022-05-24 15:13:03 +0700

    feat: calculate input and output token amount

--- tasks/calculate.ts
+++ tasks/calculate.ts
@@ -2,7 +2,7 @@ import { BigNumber } from "ethers";
 import { task } from "hardhat/config";
 import { TaskArguments } from "hardhat/types";
 
-task("calculate", "Get pair")
+task("calculateoutput", "Get pair")
   .addParam("pair", "Token A address")
   .addParam("from", "Swap from")
   .setAction(async function (taskArguments: TaskArguments, hre) {
@@ -19,9 +19,9 @@ task("calculate", "Get pair")
 
     const eth = BigNumber.from("1000000000000000000");
 
-    const fee = BigNumber.from("2000000000000000");
+    const fee = BigNumber.from("3000000000000000");
     const multiplier = eth.sub(fee);
-    const amount = eth.mul(100);
+    const amount = eth.mul(1);
     const amountWithFee = amount.mul(multiplier).div(eth);
     const constantProduct = reserveInputInitial.mul(reserveOutputInitial);
     const reserveOutputAfterExecution = constantProduct.div(reserveInputInitial.add(amountWithFee));
@@ -31,6 +31,44 @@ task("calculate", "Get pair")
     const priceImpact = eth.sub(midPrice.mul(eth).div(marketPrice));
 
     console.log(`amount: ${amount}`);
+    console.log(`amountWithFee: ${amountWithFee}`);
+    console.log(`constantProduct: ${constantProduct}`);
+    console.log(`reserveOutputAfterExecution: ${reserveOutputAfterExecution}`);
+    console.log(`amountOut: ${amountOut}`);
+    console.log(`marketPrice: ${marketPrice}`);
+    console.log(`midPrice: ${midPrice}`);
+    console.log(`priceImpact: ${priceImpact}`);
+  });
+
+task("calculateinput", "Get pair")
+  .addParam("pair", "Token A address")
+  .addParam("from", "Swap from")
+  .setAction(async function (taskArguments: TaskArguments, hre) {
+    const contract = await hre.ethers.getContractAt("IUniswapV2Pair", taskArguments.pair);
+
+    const reserves = await contract.getReserves();
+
+    const reserveInputInitial = taskArguments.from == 0 ? reserves.reserve0 : reserves.reserve1;
+
+    const reserveOutputInitial = taskArguments.from == 0 ? reserves.reserve1 : reserves.reserve0;
+
+    console.log(String(reserveInputInitial));
+    console.log(String(reserveOutputInitial));
+
+    const eth = BigNumber.from("1000000000000000000");
+
+    const fee = BigNumber.from("3000000000000000");
+    const amountOut = eth.mul(1);
+    const amountWithFee = amountOut
+      .mul(eth)
+      .mul(reserveInputInitial)
+      .div(amountOut.sub(reserveOutputInitial).mul(fee.sub(eth)));
+    const constantProduct = reserveInputInitial.mul(reserveOutputInitial);
+    const reserveOutputAfterExecution = constantProduct.div(reserveInputInitial.add(amountWithFee));
+    const marketPrice = amountWithFee.mul(eth).div(amountOut); //in uniswap it's calculated without fee!
+    const midPrice = reserveInputInitial.mul(eth).div(reserveOutputInitial);
+    const priceImpact = eth.sub(midPrice.mul(eth).div(marketPrice));
+
     console.log(`amountWithFee: ${amountWithFee}`);
     console.log(`constantProduct: ${constantProduct}`);
     console.log(`reserveOutputAfterExecution: ${reserveOutputAfterExecution}`);
